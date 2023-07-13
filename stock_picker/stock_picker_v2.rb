d5d4ca17f7063edff834dc064dfb4c6faa6b32e4
jcampbell57 <jcampbell57@gmail.com>
2023-07-12 20:56:10 -0700

    finished stock_picker exercise

---
+++ stock_picker/stock_picker_v2.rb
@@ -0,0 +1,26 @@
+def stock_picker(array)
+  pick = {}
+
+  pick[:buy] = []
+  pick[:buy][0] = 0
+  pick[:buy][1] = array[0]
+
+  pick[:sell] = []
+  pick[:sell][0] = 1
+  pick[:sell][1] = array[1]
+
+  profit = pick[:sell][1] - pick[:buy][1]
+
+  array.each_with_index do |price, index|
+    next if index == 0
+
+    next unless (price - pick[:buy][1]) >= profit
+
+    pick[:buy] = pick[:sell] if pick[:sell][1] < pick[:buy][1]
+    pick[:sell] = [index, price]
+    profit = pick[:sell][1] - pick[:buy][1]
+  end
+  puts "[#{pick[:buy][0]},#{pick[:sell][0]}]  # for a profit of $#{pick[:sell][1]} - $#{pick[:buy][1]} == $#{profit}"
+end
+
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])

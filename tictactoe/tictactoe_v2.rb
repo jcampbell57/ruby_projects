806bf9700191f5e938ecbd516f82310dd5e7f579
jcampbell57 <jcampbell57@gmail.com>
2023-07-15 21:43:10 -0700

    finishes tic tac toe v2

--- tictactoe/tictactoe_v2.rb
+++ tictactoe/tictactoe_v2.rb
@@ -4,13 +4,21 @@ class Game
 
   # initialization methods
 
-  def initialize(board = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  def initialize(board = %w[1 2 3 4 5 6 7 8 9],
+                 computer_options = [1, 2, 3, 4, 5, 6, 7, 8, 9],
                  mode = prompt_mode,
                  marker = 'X')
     self.board = board
+    self.computer_options = computer_options
     self.mode = mode
     self.marker = marker
+    prompt_player_move
   end
+
+  protected
+
+  attr_writer :board, :mode, :marker
+  attr_accessor :computer_options
 
   def prompt_mode
     puts '1 player or 2?'
@@ -24,26 +32,37 @@ class Game
       2
     else
       puts "please enter '1' or '2' for number of players"
-      check_mode_input(gets.chomp.to_i)
+      validate_mode_input(gets.chomp.to_i)
     end
   end
 
-  protected
-
-  attr_writer :board, :mode, :marker
-
   # game methods
-
-  def process_move(input); end
 
   def prompt_player_move
     display_board
     print "#{marker}'s turn! Choose a space on the board: "
-    validate_player_move(gets.chomp.to_i)
+    process_move(validate_player_move(gets.chomp.to_i))
+  end
+
+  def process_move(input)
+    board[input - 1] = marker
+    set_marker
+    Game.new if winner_declared?
+    computer_options.delete(input)
+    computer_move if mode == 1 && marker == 'O'
+    prompt_player_move
+  end
+
+  def computer_move
+    print 'Computer chooses... '
+    computer_choice = computer_options.sample
+    sleep 0.5
+    puts computer_choice
+    process_move(computer_choice)
   end
 
   def validate_player_move(input)
-    if input.match(/[0-9]/)
+    if input.to_s.match(/[1-9]/) && input.to_s.size == 1
       return input unless board[input - 1] == 'X' || board[input - 1] == 'Y'
 
       puts 'That spot is taken, pick again!'
@@ -55,13 +74,13 @@ class Game
   end
 
   def display_board
-    puts board[0..2]
-    puts board[3..5]
-    puts board[6..8]
+    p board[0..2]
+    p board[3..5]
+    p board[6..8]
   end
 
   def set_marker
-    self.marker = marker == 'X' ? 'O' : 'X'
+    self.marker = (marker == 'X' ? 'O' : 'X')
   end
 
   # winner selection methods
@@ -70,27 +89,28 @@ class Game
                     [0, 3, 6], [1, 4, 7], [2, 5, 8],
                     [0, 4, 8], [2, 4, 6]]
 
-  def declare_winner
+  def winner_declared?
     WINNING_ARRAYS.each do |winning_array|
-      return 'X wins!' if winner?(winning_array, 'X')
-      return 'Y wins!' if winner?(winning_array, 'O')
+      if winner?(winning_array, 'X')
+        display_board
+        puts 'X wins!'
+        return true
+      elsif winner?(winning_array, 'O')
+        display_board
+        puts 'O wins!'
+        return true
+      end
     end
+    return false unless board.count('X') + board.count('O') == 9
+
+    display_board
+    puts "It's a tie!"
+    true
   end
 
   def winner?(winning_array, marker)
     (board[winning_array[0]] == marker && board[winning_array[1]] == marker && board[winning_array[2]] == marker)
   end
-
-  # def old_winner?(marker)
-  #   (board[0] == marker && board[1] == marker && board[2] == marker) ||
-  #     (board[3] == marker && board[4] == marker && board[5] == marker) ||
-  #     (board[6] == marker && board[7] == marker && board[8] == marker) ||
-  #     (board[0] == marker && board[3] == marker && board[6] == marker) ||
-  #     (board[1] == marker && board[4] == marker && board[7] == marker) ||
-  #     (board[2] == marker && board[5] == marker && board[8] == marker) ||
-  #     (board[0] == marker && board[4] == marker && board[8] == marker) ||
-  #     (board[2] == marker && board[4] == marker && board[6] == marker)
-  # end
 end
 
 Game.new

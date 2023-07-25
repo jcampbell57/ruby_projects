047eb45a78d64511874d6661cf17a30ed9704846
jcampbell57 <jcampbell57@gmail.com>
2023-07-25 14:03:51 -0700

    add linkedlist file

---
+++ linked_list/linked_list.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# linkedlist class
+class LinkedList
+  def initialize; end
+end
+
+# node class
+class Node
+  def initialize(value = nil, next_node = nil)
+    self.value = value
+    self.next_node = next_node
+  end
+end

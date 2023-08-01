5b43ce0262be621a86afa32650c504b78ad76805
jcampbell57 <jcampbell57@gmail.com>
2023-08-01 11:14:48 -0700

    delete method completed

--- binary_search_trees/binary_search_trees.rb
+++ binary_search_trees/binary_search_trees.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# node class logic
+# node class
 class Node
   include Comparable
 
@@ -13,6 +13,7 @@ class Node
   end
 end
 
+# tree class
 class Tree
   attr_accessor :root
 
@@ -21,30 +22,26 @@ class Tree
   end
 
   def build_tree(array)
-    if array.length == 0
+    if array.empty?
       nil
     elsif array.length == 1
-      node = Node.new(array[0])
+      Node.new(array[0])
     else
       mid = (array.length - 1) / 2
       node = Node.new(array[mid])
-      node.left = build_tree(array[0..(mid - 1)]) unless mid == 0
-      node.right = build_tree(array[(mid + 1)..-1])
+      node.left = build_tree(array[0..(mid - 1)]) unless mid.zero?
+      node.right = build_tree(array[(mid + 1)..])
       node
     end
   end
 
-  def insert(value)
-    self.root = recursive_insert(root, value)
-  end
-
-  def recursive_insert(root, value)
+  def insert(value, root = self.root)
     if root.nil?
       root = Node.new(value)
     elsif value < root.data
-      root.left = recursive_insert(root.left, value)
+      root.left = insert(value, root.left)
     elsif value > root.data
-      root.right = recursive_insert(root.right, value)
+      root.right = insert(value, root.right)
     else
       puts 'This is strange'
       exit
@@ -52,7 +49,41 @@ class Tree
     root
   end
 
-  def delete(value); end
+  def delete(value, root = self.root)
+    if root.nil?
+      root
+    # find node to be deleted
+    elsif root.data > value
+      root.left = delete(value, root.left)
+      root
+    elsif root.data < value
+      root.right = delete(value, root.right)
+      root
+    # handle one child
+    elsif root.left.nil?
+      root.right
+    elsif root.right.nil?
+      root.left
+    # handle both children
+    else
+      old_node = root
+      # find successor node
+      new_node = root.right
+      until new_node.left.nil?
+        old_node = new_node
+        new_node = old_node.left
+      end
+
+      if old_node != root
+        old_node.left = new_node.right
+      else
+        old_node.right = new_node.right
+      end
+
+      root.data = new_node.data
+      root
+    end
+  end
 
   def find(value); end
 
@@ -82,4 +113,6 @@ end
 test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 test_tree = Tree.new(test_array)
 test_tree.insert(660)
+test_tree.pretty_print
+test_tree.delete(67)
 test_tree.pretty_print

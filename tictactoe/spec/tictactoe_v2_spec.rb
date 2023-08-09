7c0cc0c731ca7cb8cfaf17414250f491c9a83a08
jcampbell57 <jcampbell57@gmail.com>
2023-08-08 19:30:30 -0700

    create spec file and list methods

---
+++ tictactoe/spec/tictactoe_v2_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# spec/tictactoe_v2_spec.rb
+require './lib/tictactoe_v2'
+
+describe '#initialize' do
+  # no test needed
+end
+
+describe '#prompt_mode' do
+  # no test needed
+end
+
+describe '#validate_mode_input' do
+  xit '' do
+  end
+end
+
+describe '#prompt_player_move' do
+  # no test needed
+end
+
+describe '#process_move' do
+  xit '' do
+  end
+end
+
+describe '#computer_move' do
+  xit '' do
+  end
+end
+
+describe '#validate_player_move' do
+  xit '' do
+  end
+end
+
+describe '#display_board' do
+  xit '' do
+  end
+end
+
+describe '#set_marker' do
+  xit '' do
+  end
+end
+
+describe '#winner_declared?' do
+  xit '' do
+  end
+end
+
+describe '#winner?' do
+  xit '' do
+  end
+end

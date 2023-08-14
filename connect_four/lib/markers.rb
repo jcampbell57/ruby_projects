86043e013a6287f9c3aba0bd0880aa5bf56f49c6
jcampbell57 <jcampbell57@gmail.com>
2023-08-14 13:52:51 -0700

    clearer naming

--- connect_four/lib/markers.rb
+++ connect_four/lib/markers.rb
@@ -1,16 +1,16 @@
 module Markers
   # pink_circle
-  def second_mark
+  def second_marker
     "\e[38;5;213m\u25cf\e[0m"
   end
 
   # green circle
-  def player_mark
+  def player_marker
     "\e[38;5;84m\u25cf\e[0m"
   end
 
   # empty_circle
-  def empty_mark
+  def empty_marker
     "\u25cb"
   end
 end
